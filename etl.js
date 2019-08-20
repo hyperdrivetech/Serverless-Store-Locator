634cba677306704f6ea50457b0bc1cefc5d61831
Stanley Zheng <stanley.zheng@cloudreach.com>
2019-08-20 03:42:04 -0400

    init commit for insertion etl process

--- etl.js
+++ etl.js
@@ -2,9 +2,99 @@ const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
  
-fs.createReadStream('data/store-locations.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    console.log(results.slice(5));
-  });
+
+  
+
+const knex = require('knex');
+const knexPostgis = require('knex-postgis');
+ 
+const environment = process.env.ENVIRONMENT || 'development'
+const config = require('./knexfile.js')[environment];
+
+const db = knex(config);
+
+// Check if table exists if not create
+db.schema.dropTable('stores');
+db.schema.hasTable('stores').then(function(exists) {
+    // if (!exists) {
+      db.schema.createTable('stores', function(t) {
+        t.increments('id').primary();
+        t.string('store_name', 100);
+        t.string('store_location', 100);
+        t.text('address');
+        t.string('city', 100);
+        t.string('state', 100);
+        t.string('zip_code', 100);
+        t.specificType("geom", "geometry(point, 4326)");
+        t.string('county', 100);
+      });
+    //   return fs.createReadStream('seed/store-locations.csv')
+    // return fs.createReadStream('seed/test.csv')
+    //   .pipe(csv({
+    //     mapHeaders: ({ header, index }) => header.toLowerCase()
+    //   }))
+    //   .on('data', (data) => results.push(data))
+    //   .on('end', () => {
+        
+    //     db.insert({
+    //         geom: st.geomFromText('POINT(-71.064544 42.28787)', 4326)
+    //       }).into('points').toString();
+    //   });
+      
+    // }
+});
+
+fs.createReadStream('seed/test.csv')
+      .pipe(csv({
+        mapHeaders: ({ header, index }) => header.toLowerCase().replace(' ', '_')
+      }))
+      .on('data', (row) => {
+          let data = Object.assign({}, row)
+        //   data.geom = `POINT(${data.latitude}, ${data.longitude})`
+          data.geom = st.geomFromText(`POINT(${data.latitude}, ${data.longitude})`, 4326)
+          sql = db.insert(data).into('stores')
+          console.log(sql)
+          results.push(row)
+        })
+      .on('end', () => {
+        console.log(results)
+      });
+
+db.schema.hasTable('points').then(function(exists) {
+    if (!exists) {
+      return db.schema.createTable('points', function(t) {
+        t.increments('id').primary();
+        t.specificType("geom", "geometry(point, 4326)");
+      });
+    }
+});
+
+
+  
+ 
+// install postgis functions in knex.postgis;
+const st = knexPostgis(db);
+/* or:
+ * knexPostgis(db);
+ * const st = db.postgis;
+ */
+ 
+// insert a point
+const sql1 = db.insert({
+  geom: st.geomFromText('POINT(-71.064544 42.28787)', 4326)
+}).into('points').toString();
+console.log(sql1);
+// insert into "points" ("geom", "id") values (ST_geomFromText('Point(0 0)'), '1')
+ 
+// find all points return point in wkt format
+// const sql2 = 
+// console.log('res:', sql2);
+// select "id", ST_asText("geom") as "geom" from "points"
+
+
+ x = (async ()=>{
+    res = await db.select('id', st.asText('geom')).from('points');
+    console.log(res)
+ })
+
+ x()
---
+++ knexfile.js
@@ -0,0 +1,39 @@
+// Update with your config settings.
+
+module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: '127.0.0.1',
+      database: 'postgres',
+      user:     'postgres',
+      password: 'mysecretpassword'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres',
+      user:     'postgres',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
+
+};
