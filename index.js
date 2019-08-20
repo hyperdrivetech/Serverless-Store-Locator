177ff3d3f9cda22ec55c6786049a5603f66151de
Stanley Zheng <stanley.zheng@cloudreach.com>
2019-08-20 19:58:44 -0400

    clean up error and handling work

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ exports.closest = async (request, response) => {
       .send({ "status:": 400, msg: "no related address/zipcode found" });
   }
 
-  console.log(zip, address, units);
   return response.status(200).send({ status: 200, msg: res });
 };
 
@@ -85,7 +84,6 @@ async function find_via_address(address, units) {
     .where(st.dwithin(knex.raw("geom::geography"), origin, distance.toFixed(0)))
     .orderBy("distance")
     .limit(10);
-  // console.log(dis, dis[0] ? dis.length > 1 : [])
 
   if (res.length > 1) {
     res[0]["distance"] = res[0]["distance"] * 1000;
@@ -109,52 +107,3 @@ function query_google(address) {
       return err;
     });
 }
-
-var data = [
-  {
-    address_components: [
-      { long_name: "1600", short_name: "1600", types: ["street_number"] },
-      {
-        long_name: "Amphitheatre Parkway",
-        short_name: "Amphitheatre Pkwy",
-        types: ["route"]
-      },
-      {
-        long_name: "Mountain View",
-        short_name: "Mountain View",
-        types: ["locality", "political"]
-      },
-      {
-        long_name: "Santa Clara County",
-        short_name: "Santa Clara County",
-        types: ["administrative_area_level_2", "political"]
-      },
-      {
-        long_name: "California",
-        short_name: "CA",
-        types: ["administrative_area_level_1", "political"]
-      },
-      {
-        long_name: "United States",
-        short_name: "US",
-        types: ["country", "political"]
-      },
-      { long_name: "94043", short_name: "94043", types: ["postal_code"] }
-    ],
-    formatted_address: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
-    geometry: {
-      location: { lat: 37.4260076, lng: -122.0911298 },
-      location_type: "ROOFTOP",
-      viewport: {
-        northeast: { lat: 37.4273565802915, lng: -122.0897808197085 },
-        southwest: { lat: 37.4246586197085, lng: -122.0924787802915 }
-      }
-    },
-    place_id: "ChIJtYuu0V25j4ARwu5e4wwRYgE",
-    plus_code: {
-      compound_code: "CWG5+CG Mountain View, California, United States",
-      global_code: "849VCWG5+CG"
-    },
-    types: ["street_address"]
-  }
-];
--- index.spec.js
+++ index.spec.js
@@ -2,6 +2,56 @@ var assert = require('assert');
 var expect = require('chai').expect;
 var should = require('chai').should();
 
+var data = [
+    {
+      address_components: [
+        { long_name: "1600", short_name: "1600", types: ["street_number"] },
+        {
+          long_name: "Amphitheatre Parkway",
+          short_name: "Amphitheatre Pkwy",
+          types: ["route"]
+        },
+        {
+          long_name: "Mountain View",
+          short_name: "Mountain View",
+          types: ["locality", "political"]
+        },
+        {
+          long_name: "Santa Clara County",
+          short_name: "Santa Clara County",
+          types: ["administrative_area_level_2", "political"]
+        },
+        {
+          long_name: "California",
+          short_name: "CA",
+          types: ["administrative_area_level_1", "political"]
+        },
+        {
+          long_name: "United States",
+          short_name: "US",
+          types: ["country", "political"]
+        },
+        { long_name: "94043", short_name: "94043", types: ["postal_code"] }
+      ],
+      formatted_address: "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
+      geometry: {
+        location: { lat: 37.4260076, lng: -122.0911298 },
+        location_type: "ROOFTOP",
+        viewport: {
+          northeast: { lat: 37.4273565802915, lng: -122.0897808197085 },
+          southwest: { lat: 37.4246586197085, lng: -122.0924787802915 }
+        }
+      },
+      place_id: "ChIJtYuu0V25j4ARwu5e4wwRYgE",
+      plus_code: {
+        compound_code: "CWG5+CG Mountain View, California, United States",
+        global_code: "849VCWG5+CG"
+      },
+      types: ["street_address"]
+    }
+  ];
+  
+
 it('should return true if valid', function(){
       var isValid = true;
       //assert.equal(isValid, true);
