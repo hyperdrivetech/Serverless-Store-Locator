be0e987485d6a2c43c620364005a539c78246dc6
Stanley Zheng <stanley.zheng@cloudreach.com>
2019-08-22 07:19:57 -0400

    adding seed schema

---
+++ seed/store-schema.sql
@@ -0,0 +1,18 @@
+-- Drop table
+
+-- DROP TABLE public.stores;
+
+CREATE TABLE public.stores (
+	id serial NOT NULL,
+	store_name varchar(100) NULL,
+	store_location varchar(100) NULL,
+	address text NULL,
+	city varchar(100) NULL,
+	state varchar(100) NULL,
+	zip_code varchar(100) NULL,
+	geom geometry(POINT, 4326) NULL,
+	latitude varchar(100) NULL,
+	longitude varchar(100) NULL,
+	county varchar(100) NULL,
+	CONSTRAINT stores_pkey PRIMARY KEY (id)
+);
